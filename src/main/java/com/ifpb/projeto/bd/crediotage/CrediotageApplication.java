59fd049efaf4c180a4c0c6e0d867815af362294d
yasminsarinho <yasmiinsainho@gmail.com>
2025-04-23 11:21:02 -0300

    Aentity base criadas
    :

--- src/main/java/com/ifpb/projeto/bd/crediotage/CrediotageApplication.java
+++ src/main/java/com/ifpb/projeto/bd/crediotage/CrediotageApplication.java
@@ -1,28 +1,13 @@
 package com.ifpb.projeto.bd.crediotage;
 
-import com.ifpb.projeto.bd.crediotage.model.Usuario;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class CrediotageApplication {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args){
 		SpringApplication.run(CrediotageApplication.class, args);
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("crediotage");
-		EntityManager em = emf.createEntityManager();
-		Usuario usuario = new Usuario("Yasmin", "123123123", "senha", "endereco", LocalDate.now());
-		em.getTransaction().begin();
-		em.persist(usuario);
-		em.getTransaction().commit();
-		emf.close();
-		em.close();
 	}
 
 }
